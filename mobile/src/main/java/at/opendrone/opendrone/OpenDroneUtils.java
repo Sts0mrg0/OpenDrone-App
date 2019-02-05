85fb23873e4fb5dd92e697b366635ac5db5a5b55
MarkusKurzmann <20150081@HTBLAPerg.onmicrosoft.com>
2019-02-05 10:23:24 +0100

    Implemented kind of settings but they still ugly tho

--- mobile/src/main/java/at/opendrone/opendrone/OpenDroneUtils.java
+++ mobile/src/main/java/at/opendrone/opendrone/OpenDroneUtils.java
@@ -14,6 +14,9 @@ public class OpenDroneUtils {
     public static String SP_FLIGHTPLAN_NAME = "sp_flightplan_name";
     public static String SP_FLIGHTPLAN_DESC = "sp_flightplan_desc";
     public static String SP_FLIGHTPLAN_POSITION = "sp_flightplan_pos";
+    public static String SP_SETTINGS_LANGUAGE = "sp_settings_language";
+    public static String SP_SETTINGS_PROMODE = "sp_settings_promode";
+    public static String SP_SETTINGS_MAXHEIGHT = "sp_settings_maxHeight";
     public static final double DEFAULT_LAT = 48.2468036;
     public static final double DEFAULT_LNG = 14.6199875;
     public static final String THROTTLE_UP = "THRT_UP";
@@ -29,6 +32,7 @@ public class OpenDroneUtils {
     public static final int LF_DRONE = 1;
     public static final int LF_FP = 2;
     public static final int LF_FLY = 3;
+    public static final int LF_SETTINGS = 4;
 
     public static final byte CODE_THROTTLE_UP= (byte) 1;
     public static final byte CODE_THROTTLE_DOWN= (byte) 2;
